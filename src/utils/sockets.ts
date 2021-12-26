982df5a1f4f18dbee0318973643ac7050836bf76
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2021-12-26 15:44:39 -0300

    Add env variables

--- src/utils/sockets.ts
+++ src/utils/sockets.ts
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socket = io();
+const socket = process.env.REACT_APP_STAGE === 'prod'
+  ? io()
+  : io(String(process.env.REACT_APP_SOCKET_URL), { transports: ['websocket'] });
 
 export default socket;
